1ed7b1bd85086047bc3182c40e4bd3d6c72b3355
ricardo@sierratecnologia.com.br <ricardo@sierratecnologia.com.br>
2020-12-21 17:04:52 -0300

    Fix bugs

--- src/Http/Controllers/User/PorteiroUserController.php
+++ src/Http/Controllers/User/PorteiroUserController.php
@@ -5,7 +5,7 @@ namespace Porteiro\Http\Controllers\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Porteiro\Facades\Porteiro;
-use Support\Routing\UrlGenerator;
+use Pedreiro\Routing\UrlGenerator;
 use Porteiro\Http\Controllers\User\Controller;
 
 class PorteiroUserController extends Controller
@@ -18,7 +18,7 @@ class PorteiroUserController extends Controller
             $route = route('facilitador.users.edit', Auth::user()->getKey());
         } elseif ($dataType) {
             $route = UrlGenerator::managerRoute($dataType->slug, 'edit', Auth::user()->getKey());
-            // $route = \Support\Routing\UrlGenerator::managerRoute($dataType->slug, 'edit', Auth::user()->getKey());
+            // $route = \Pedreiro\Routing\UrlGenerator::managerRoute($dataType->slug, 'edit', Auth::user()->getKey());
         }
 
         return Porteiro::view('facilitador::profile', compact('route'));
