310f2688abbec6fdb93bd63872a88d3b9bb473cb
ricardo@sierratecnologia.com.br <ricardo@sierratecnologia.com.br>
2021-02-18 05:17:50 -0300

    CAMN-10

--- src/Http/Controllers/Admin/DashboardController.php
+++ src/Http/Controllers/Admin/DashboardController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Schema;
 use Spatie\Analytics\Analytics;
 use Spatie\Analytics\Period;
 use Tracking\Services\AnalyticsService;
+use Pedreiro\Components\BoxComponent;
 
 class DashboardController extends Controller
 {
@@ -24,22 +25,128 @@ class DashboardController extends Controller
     public function index()
     {
         $title = "Home";
+        $boxComponents = [];
 
-        $articles = 0; //Blog::count();
-        $pages = 0; //Page::count();
-        $members = 0; //UserMeta::count();
-        // $subscriptions = Subscription::count();
-        $photos = 0; //Photo::count();
+        
+        if (class_exists(\Cms\Models\Blog\Blog::class)){
+
+            $boxComponents[] = BoxComponent::create(
+                // color
+                'red',
+                // number
+                \Cms\Models\Blog\Blog::count(),
+                // name
+                trans('words.articles'),
+                // icon
+                'fa fa-user-plus',
+                // linkText
+                trans('words.moreInfo'),
+                // link
+                url('admin/articles')
+            );
+        } else if (class_exists(\Siravel\Models\Blog\Blog::class)){
+            $boxComponents[] = BoxComponent::create(
+                // color
+                'red',
+                // number
+                \Siravel\Models\Blog\Blog::count(),
+                // name
+                trans('words.articles'),
+                // icon
+                'fa fa-user-plus',
+                // linkText
+                trans('words.moreInfo'),
+                // link
+                url('admin/articles')
+            );
+        }
+
+        if (class_exists(\Cms\Models\Negocios\Page::class)){
+            $boxComponents[] = BoxComponent::create(
+                // color
+                'yellow',
+                // number
+                \Cms\Models\Negocios\Page::count(),
+                // name
+                trans('words.pages'),
+                // icon
+                'fa fa-key',
+                // linkText
+                trans('words.moreInfo'),
+                // link
+                url('admin/pages')
+                
+            );
+        } else if (class_exists(\Siravel\Models\Negocios\Page::class)){
+            $boxComponents[] = BoxComponent::create(
+                // color
+                'yellow',
+                // number
+                \Siravel\Models\Negocios\Page::count(),
+                // name
+                trans('words.pages'),
+                // icon
+                'fa fa-key',
+                // linkText
+                trans('words.moreInfo'),
+                // link
+                url('admin/pages')
+            );
+        }
+
+        if (class_exists(\Cms\Models\Negocios\Subscription::class)){
+            $boxComponents[] = BoxComponent::create(
+                // color
+                'aqua',
+                // number
+                \Cms\Models\Negocios\Subscription::count(),
+                // name
+                trans('words.members'),
+                // icon
+                'ion ion-person-stalker',
+                // linkText
+                trans('words.moreInfo'),
+                // link
+                url('admin/members')
+            );
+        } else if (class_exists(\Siravel\Models\Negocios\Subscription::class)){
+            $boxComponents[] = BoxComponent::create(
+                // color
+                'yellow',
+                // number
+                \Siravel\Models\Negocios\Subscription::count(),
+                // name
+                trans('words.pages'),
+                // icon
+                'fa fa-key',
+                // linkText
+                trans('words.moreInfo'),
+                // link
+                url('admin/pages'),
+            );
+        }
+
+        if (class_exists(\Stalker\Models\Photo::class)){
+            $boxComponents[] = BoxComponent::create(
+                // color
+                'aqua',
+                // number
+                \Stalker\Models\Photo::count(),
+                // name
+                trans('words.photos'),
+                // icon
+                'ion ion-person-stalker',
+                // linkText
+                trans('words.moreInfo'),
+                // link
+                url('admin/photos')
+            );
+        }
 
         return view(
-            'porteiro::admin.dashboard.home',
+            'porteiro::components.dashboard',
             compact(
-                'title',
-                'articles',
-                'pages',
-                'photos',
-                'members',
-                // 'subscriptions'
+                'boxComponents',
             )
         );
     }
